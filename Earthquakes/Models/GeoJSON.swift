26f5de2e756ae0c26ab3918eb67497dac1bb773e
Teff <110036529+runnneverstop722@users.noreply.github.com>
2023-06-06 16:30:57 +0900

    Decode GeoJSON - Modeling the GeoJSON hierarchy - Access an array of `features` which is embedded two layers deep among other data(not gonna use them) - To decode the array of the `GeoJSON features`, use Quake model to decode each element of the array

---
+++ Earthquakes/Models/GeoJSON.swift
@@ -0,0 +1,9 @@
+//
+//  GeoJSON.swift
+//  Earthquakes-iOS
+//
+//  Created by Teff on 2023/05/31.
+//  Copyright © 2023 Apple. All rights reserved.
+//
+
+import Foundation
---
+++ EarthquakesTests/EarthquakesTests.swift
@@ -0,0 +1,9 @@
+//
+//  EarthquakesTests.swift
+//  EarthquakesTests
+//
+//  Created by Teff on 2023/05/31.
+//  Copyright © 2023 Apple. All rights reserved.
+//
+
+import Foundation
