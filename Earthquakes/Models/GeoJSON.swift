e7407cec139f2b8865ea2322b5069e9c49411909
Teff <110036529+runnneverstop722@users.noreply.github.com>
2023-06-07 20:10:49 +0900

    - Decode GeoJSON and a Location

--- Earthquakes/Models/GeoJSON.swift
+++ Earthquakes/Models/GeoJSON.swift
@@ -7,3 +7,27 @@
 //
 
 import Foundation
+
+struct GeoJSON: Decodable {
+    private enum RootCodingKeys: String, CodingKey {
+        case features
+    }
+    private enum FeatureCodingKeys: String, CodingKey {
+        case properties
+    }
+    private(set) var quakes: [Quake] = [] // Code within the GeoJSON structure can modify the quakes property, but code outside the structure can only read the property value
+    
+    init(from decoder: Decoder) throws {
+        let rootContainer = try decoder.container(keyedBy: RootCodingKeys.self)
+        var featureContainer = try rootContainer.nestedUnkeyedContainer(forKey: .features)
+        
+        while !featureContainer.isAtEnd {
+            let propertiesContainer = try featureContainer.nestedContainer(keyedBy: FeatureCodingKeys.self)
+            
+            // Using try? ignores quakes with missing data
+            if let properties = try? propertiesContainer.decode(Quake.self, forKey: .properties) {
+                quakes.append(properties)
+            }
+        }
+    }
+}
